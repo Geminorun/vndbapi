7b7b5358b1b2b9d003f047aec6a459bbd11a470d
LYL <yulin_luo@126.com>
2023-11-25 18:04:26 +0800

    1.对接Moon

---
+++ src/test/java/com/ymgal/HarvestTests.java
@@ -0,0 +1,18 @@
+package com.ymgal;
+
+import com.ymgal.dto.HarvestResult;
+
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class HarvestTests {
+
+    public static void main(String[] args) throws Exception {
+        //clannad
+        HarvestResult vndbHarvest = VndbHarvest.getVndbHarvest(17);
+
+        //return harvestResult;
+
+    }
+}
